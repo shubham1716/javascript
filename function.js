81e6828a084c282899bf52be9dcbbd004c08849c
shubham1716 <shubhamk77038@gmail.com>
2025-12-25 03:24:16 -0500

    use object in function

--- function.js
+++ function.js
@@ -1,10 +1,38 @@
-function addtwo(num1 ,num2 ){
-    console.log( num1 + num2);
+// function addtwo(num1 ,num2 ){
+//     console.log( num1 + num2);
+// }
+
+// addtwo(5, 10);  
+
+// function add(num1 ,num2 ){
+//     console.log( num1 + num2);
+// }
+// add(15, 25);
+
+function calculatecartprice(num1){
+    return num1;
+
 }
 
-addtwo(5, 10);  
+console.log(calculatecartprice(1000))
 
-function add(num1 ,num2 ){
-    console.log( num1 + num2);
+////    ...    rest operator
+function calculatecartprice(...num1){
+    return num1;
+
 }
-add(15, 25);
+
+console.log(calculatecartprice(1000,10,200))
+
+
+const user = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+function handleobject(anyobject){
+    console.log(`Name: ${anyobject.name}, Age: ${anyobject.age}, City: ${anyobject.city}`);
+}
+
+handleobject(user);
