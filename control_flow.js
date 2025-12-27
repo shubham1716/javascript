258f52548d4d17f61dd25b000409a40bd393c796
shubham1716 <shubhamk77038@gmail.com>
2025-12-27 01:56:51 -0500

    control statement

---
+++ control_flow.js
@@ -0,0 +1,43 @@
+ if(2=='2'){
+    console.log("Equal");
+ }
+ else{
+    console.log("Not Equal");
+ }
+
+
+ ////
+
+ let balance = 1000;
+
+ if(balance <= 500){
+    console.log("Low balance");
+ }
+ else if(balance<700){
+    console.log("Medium balance");
+ }
+ else{
+    console.log("High balance");
+ }
+
+
+
+ ///switch case
+
+ const month = 5;
+ switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+        case 2:
+            console.log("February");
+            break;
+            case 3:
+                console.log("March");
+                break;
+ 
+    default:
+        console.log("Not a valid month");
+        break;
+ }
