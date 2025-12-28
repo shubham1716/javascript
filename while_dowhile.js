d2f70b3f4477b1c5ec4155ab2169b99ca0bd3be1
shubham1716 <shubhamk77038@gmail.com>
2025-12-28 06:02:13 -0500

    while,dowhile

---
+++ while_dowhile.js
@@ -0,0 +1,15 @@
+//while
+let value =0
+while (value <=10) {
+    console.log("Value is: " + value);
+    value++;
+}
+
+let score =1;
+
+
+do {
+    console.log(score);
+    score++;
+    
+} while (score <=5);
