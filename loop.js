647f8cacfe9c9966b11ad987bfce6efeefee4236
shubham1716 <shubhamk77038@gmail.com>
2025-12-28 05:52:04 -0500

    loop

---
+++ loop.js
@@ -0,0 +1,39 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+// for(let i=1; i<=5; i++){
+//     for(let j=0;j<=5;j++){
+//         console.log(i*j);
+//     }
+// }
+
+
+// let myarray=[1,2,3,4,5]
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }
+
+///break and continue
+
+for (let i = 0; i <=10; i++){
+    const element = [i];
+if(element==5){
+    break;
+ }
+ console.log(element);
+}
+
+for (let i = 0; i <=10; i++){
+    const element = [i];
+if(element==5){
+
+    continue;
+ }
+ console.log(element);
+}
