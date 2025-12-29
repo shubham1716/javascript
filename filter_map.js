b94366bd15ecbc2f092b3781db75587ed57d5b7b
shubham1716 <shubhamk77038@gmail.com>
2025-12-29 02:44:05 -0500

    filter

---
+++ filter_map.js
@@ -0,0 +1,13 @@
+// const coding = ['JavaScript', 'C Plus Plus', 'Python', 'Java', 'Ruby'];
+
+
+// const value=coding.forEach(function(lang, index) {
+//     console.log(index + " : " + lang);
+// })
+// console.log(value);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const nums =numbers.filter((num)=> num > 4)
+
+console.log(nums)
